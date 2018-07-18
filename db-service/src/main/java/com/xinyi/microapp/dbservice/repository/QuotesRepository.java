54e691b387f23e1a9fb7e54244deaad763dd55e2
xinyi <xinyi2017mm@gmail.com>
2018-07-18 15:59:26 -0700

    Database service, code finish. Netflix server does not work.

---
+++ db-service/src/main/java/com/xinyi/microapp/dbservice/repository/QuotesRepository.java
@@ -0,0 +1,10 @@
+package com.xinyi.microapp.dbservice.repository;
+
+import com.xinyi.microapp.dbservice.model.Quote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+    List<Quote> findByUserName(String username);
+}
